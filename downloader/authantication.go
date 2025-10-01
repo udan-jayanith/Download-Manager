84fc53528ab21a9a9715d7ad30195179d0c481c6
Udan Jayanith Jayakody <j.udanjayanith@gmail.com>
2025-10-01 22:13:57 +0530

    Chnages

--- downloader/authantication.go
+++ downloader/authantication.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 	"math/rand/v2"
 	"net/http"
 	"os"
@@ -10,14 +11,45 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-func newToken() (token string) {
-	for range 10 {
+type TokenStruct struct{}
+
+func newToken() TokenStruct {
+	token := TokenStruct{}
+	err := Sqlite.Execute(func(db *sqlx.DB) error {
+		_, err := db.Exec(`
+			CREATE TABLE IF NOT EXISTS token (Token TEXT NOT NULL);
+		`)
+		return err
+	})
+	if err != nil {
+		log.Println("token table SQL execution error")
+		log.Fatal(err)
+	}
+
+	var rowCount int
+	err = Sqlite.Execute(func(db *sqlx.DB) error {
+		row := db.QueryRow(`
+			SELECT COUNT(Token) FROM token;
+		`)
+		return row.Scan(&rowCount)
+	})
+	if err != nil {
+		log.Println("token table SQL execution error")
+		log.Fatal(err)
+	} else if rowCount <= 0 {
+		token.saveToken(token.newToken())
+	}
+	return token
+}
+
+func (_ *TokenStruct) newToken() (token string) {
+	for range 60 {
 		token += string(rune('0') + (rand.Int32() % (rune('z') - rune('0'))))
 	}
 	return token
 }
 
-func getToken() (token string, err error) {
+func (_ *TokenStruct) getToken() (token string, err error) {
 	err = Sqlite.Execute(func(db *sqlx.DB) error {
 		row := db.QueryRow(`
 			SELECT Token FROM token LIMIT 1;
@@ -27,7 +59,7 @@ func getToken() (token string, err error) {
 	return token, err
 }
 
-func saveToken(token string) error {
+func (_ *TokenStruct) saveToken(token string) error {
 	return Sqlite.Execute(func(db *sqlx.DB) error {
 		_, err := db.Exec(`
 			INSERT INTO token (Token)
@@ -37,19 +69,30 @@ func saveToken(token string) error {
 	})
 }
 
-func HttpTokenHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:"+os.Getenv("port"))
-	w.Header().Add("Content-Type", "application/json")
-	fmt.Fprintf(w, `
-		{
-			"token": "%s"
-		}
-	`, token)
+func (_ *TokenStruct) changeToken(token string) error {
+	return Sqlite.Execute(func(db *sqlx.DB) error {
+		_, err := db.Exec(`
+			UPDATE token SET Token = ?;
+		`, token)
+		return err
+	})
+}
+
+var (
+	Token = newToken()
+)
+
+func getTokenFromReq(r *http.Request) string {
+	return strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
 }
 
 func RequireAuthenticationToken(w http.ResponseWriter, r *http.Request) (ok bool) {
-	authToken := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
-	if authToken == token {
+	token, err := Token.getToken()
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	if getTokenFromReq(r) == token {
 		return true
 	}
 
@@ -66,4 +109,47 @@ func RequireAuthenticationToken(w http.ResponseWriter, r *http.Request) (ok bool
 	}
 
 	return true
+}
+
+func HandleAuth(mux *http.ServeMux) {
+	mux.HandleFunc("/token/get", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:"+os.Getenv("port"))
+		w.Header().Add("Content-Type", "application/json")
+		token, err := Token.getToken()
+		if err != nil {
+			WriteError(w, err.Error())
+			return
+		}
+
+		res := map[string]string{
+			"token": token,
+		}
+		json.NewEncoder(w).Encode(&res)
+	})
+
+	mux.HandleFunc(`/token/change`, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:"+os.Getenv("port"))
+		w.Header().Add("Content-Type", "application/json")
+
+		token := Token.newToken()
+		err := Token.changeToken(token)
+		if err != nil {
+			WriteError(w, err.Error())
+		}
+	})
+
+	mux.HandleFunc("/token/is-valid", func(w http.ResponseWriter, r *http.Request) {
+		AllowCrossOrigin(w)
+		reqToken := r.FormValue("token")
+		token, err := Token.getToken()
+		if err != nil {
+			WriteError(w, err.Error())
+			return
+		}
+
+		res := map[string]bool{
+			"is-valid": reqToken == token,
+		}
+		json.NewEncoder(w).Encode(&res)
+	})
 }
---
+++ downloader/download-handlers.go
@@ -0,0 +1,293 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/jmoiron/sqlx"
+	_ "modernc.org/sqlite"
+)
+
+func HandleDownloads(mux *http.ServeMux) {
+	err := Sqlite.Execute(func(db *sqlx.DB) error {
+		_, err := db.Exec(`
+		CREATE TABLE IF NOT EXISTS downloads 
+		(
+		ID INTEGER PRIMARY KEY,
+		FileName TEXT NOT NULL,
+		URL TEXT NOT NULL,
+		Dir TEXT NOT NULL,
+		ContentLength INTEGER,
+		DateAndTime TEXT NOT NULL,
+		Status INTEGER NOT NULL,
+		TempFilePath TEXT
+		);`)
+		return err
+	})
+	if err != nil {
+		log.Println("downloads table SQL execution error")
+		log.Fatal(err)
+	}
+
+	err = Sqlite.Execute(func(db *sqlx.DB) error {
+		_, err := db.Exec(`
+			UPDATE downloads SET Status = ?
+			WHERE Status = ? OR Status = ?;	
+		`, Paused, Downloading, Pending)
+		return err
+	})
+	if err != nil {
+		log.Println("downloads table corruptions fix error")
+		log.Fatal(err)
+	}
+
+	waUpgrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	mux.HandleFunc("/download/wa/updates", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := waUpgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		err = updatesHandler.AddConn(conn)
+		if err != nil {
+			conn.Close()
+		}
+	})
+
+	mux.HandleFunc("/download/download", downloadHandler)
+	mux.HandleFunc("/download/get-downloads", getDownloads)
+	mux.HandleFunc("/download/get-downloading", getDownloading)
+	mux.HandleFunc("/download/search-downloads", searchDownload)
+	mux.HandleFunc("/download/pause", pauseDownload)
+	mux.HandleFunc("/download/resume", resumeDownload)
+	mux.HandleFunc("/download/delete", deleteDownload)
+}
+
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+	AllowCrossOrigin(w)
+	if !RequireAuthenticationToken(w, r) {
+		return
+	}
+
+	type request struct {
+		FileName string `json:"file-name"`
+		URL      string `json:"url"`
+		Dir      string `json:"dir"`
+	}
+
+	var req request
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		WriteError(w, fmt.Sprintf("body decoding error, %s", err))
+		return
+	}
+
+	downloadItem := NewDownloadItem(req.FileName, req.Dir, req.URL)
+	downloadWorkPool.Download(downloadItem)
+}
+
+func getDownloads(w http.ResponseWriter, r *http.Request) {
+	AllowCrossOrigin(w)
+	if !RequireAuthenticationToken(w, r) {
+		return
+	}
+
+	limit := 20
+	dateAndTime := r.FormValue("date-and-time")
+
+	Sqlite.Mutex.Lock()
+	defer Sqlite.Mutex.Unlock()
+
+	jsonRes := struct {
+		DownloadItems []DownloadItemJson `json:"download-items"`
+	}{
+		DownloadItems: make([]DownloadItemJson, 0, 20),
+	}
+
+	if strings.TrimSpace(dateAndTime) == "" {
+		err := Sqlite.DB.Select(&jsonRes.DownloadItems, `
+				SELECT * FROM downloads WHERE Status = ? ORDER BY DateAndTime ASC LIMIT ?;
+			`, Complete, limit)
+		if err != nil {
+			WriteError(w, err.Error())
+			return
+		}
+	} else {
+		err := Sqlite.DB.Select(&jsonRes.DownloadItems, `
+				SELECT * FROM downloads WHERE Status = ? AND DateAndTime > ? ORDER BY DateAndTime ASC LIMIT ?;
+			`, Complete, dateAndTime, limit)
+		if err != nil {
+			WriteError(w, err.Error())
+			return
+		}
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&jsonRes)
+}
+
+func getDownloading(w http.ResponseWriter, r *http.Request) {
+	AllowCrossOrigin(w)
+	if !RequireAuthenticationToken(w, r) {
+		return
+	}
+
+	jsonRes := struct {
+		DownloadingItems []DownloadItemJson `json:"downloading-items"`
+	}{
+		DownloadingItems: make([]DownloadItemJson, 0, 3),
+	}
+
+	err := Sqlite.Execute(func(db *sqlx.DB) error {
+		err := db.Select(&jsonRes.DownloadingItems, `
+			SELECT * FROM downloads WHERE Status != ?;
+		`, Complete)
+		if err != nil {
+			return err
+		}
+		return err
+	})
+	if err != nil {
+		WriteError(w, err.Error())
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&jsonRes)
+}
+
+func searchDownload(w http.ResponseWriter, r *http.Request) {
+	AllowCrossOrigin(w)
+	if !RequireAuthenticationToken(w, r) {
+		return
+	}
+
+	query := strings.TrimSpace(r.FormValue("query"))
+	if query == "" {
+		WriteError(w, "Missing query")
+		return
+	}
+	query = "%" + query + "%"
+
+	Sqlite.Mutex.Lock()
+	defer Sqlite.Mutex.Unlock()
+
+	searchResults := struct {
+		SearchResults []DownloadItemJson `json:"search-results"`
+	}{
+		SearchResults: make([]DownloadItemJson, 0, 20),
+	}
+
+	err := Sqlite.DB.Select(&searchResults.SearchResults, `
+		SELECT * FROM downloads WHERE Status = ? AND (FileName LIKE ? OR URL LIKE ?);
+	`, Complete, query, query)
+	if err != nil {
+		WriteError(w, err.Error())
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&searchResults)
+}
+
+func pauseDownload(w http.ResponseWriter, r *http.Request) {
+	AllowCrossOrigin(w)
+	if !RequireAuthenticationToken(w, r) {
+		return
+	}
+
+	downloadID, err := strconv.Atoi(r.FormValue("download-id"))
+	if err != nil {
+		WriteError(w, err.Error())
+		return
+	}
+
+	downloadItem, ok := downloadWorkPool.GetDownloadItem(int64(downloadID))
+	if !ok {
+		return
+	} else if !downloadItem.PartialContent {
+		WriteError(w, "Pause is not supported.")
+		return
+	}
+	downloadItem.Cancel()
+}
+
+func resumeDownload(w http.ResponseWriter, r *http.Request) {
+	AllowCrossOrigin(w)
+	if !RequireAuthenticationToken(w, r) {
+		return
+	}
+
+	downloadID, err := strconv.Atoi(r.FormValue("download-id"))
+	if err != nil {
+		WriteError(w, "Missing download-id")
+		return
+	}
+
+	var downloadItemJson DownloadItemJson
+	err = Sqlite.Execute(func(db *sqlx.DB) error {
+		return db.Get(&downloadItemJson, `
+				SELECT * FROM downloads WHERE ID = ? LIMIT 1;
+			`, downloadID)
+	})
+	if err != nil {
+		WriteError(w, err.Error())
+		return
+	} else if downloadItemJson.URL == "" {
+		WriteError(w, "URL is not found.")
+		return
+	}
+
+	downloadItem, err := downloadItemJson.ToDownloadItem()
+	if err != nil {
+		WriteError(w, err.Error())
+		return
+	}
+
+	downloadWorkPool.Download(downloadItem)
+}
+
+func deleteDownload(w http.ResponseWriter, r *http.Request) {
+	AllowCrossOrigin(w)
+	if !RequireAuthenticationToken(w, r) {
+		return
+	}
+
+	downloadId, err := strconv.Atoi(r.FormValue("download-id"))
+	if err != nil {
+		WriteError(w, err.Error())
+		return
+	}
+
+	var downloadItemJson DownloadItemJson
+	err = Sqlite.Execute(func(db *sqlx.DB) error {
+		return db.Get(&downloadItemJson, `
+				SELECT * FROM downloads WHERE ID = ? LIMIT 1;
+			`, downloadId)
+	})
+	if err != nil {
+		WriteError(w, err.Error())
+		return
+	}
+
+	downloadItem, err := downloadItemJson.ToDownloadItem()
+	if err != nil {
+		WriteError(w, err.Error())
+		return
+	}
+
+	downloadItem.Delete()
+}
