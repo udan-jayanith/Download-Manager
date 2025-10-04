7e032e822cb801de54290b1e487bd38c7cd4a05c
Udan Jayanith Jayakody <j.udanjayanith@gmail.com>
2025-10-04 16:57:41 +0530

    Changes

--- extension/webRequests.js
+++ extension/webRequests.js
@@ -1,3 +1,139 @@
+//This takes a header value and tokenizes it and returns it.
+function parseHeaderValue(headerValue) {
+	let tokens = []
+
+	let currentToken = ''
+	let isReadingQuote = false
+	for (let i = 0; i < headerValue.length; i++) {
+		if (!isReadingQuote && headerValue[i] == ' ') {
+			continue
+		}
+
+		switch (headerValue[i]) {
+			case `"`:
+				isReadingQuote = !isReadingQuote
+				break
+			case ',':
+			case `;`:
+				tokens.push(currentToken)
+				currentToken = ''
+				break
+			default:
+				currentToken += headerValue[i]
+				break
+		}
+	}
+	tokens.push(currentToken)
+	return tokens
+}
+
+function escapeFolderName(folderName) {
+	let newFolderName = ''
+	let invalidChar = new Set(['/', '\\'[0], '=', '"', ',', ':', '*', '+', '<', '>', '|', '.'])
+	for (let i = 0; i < folderName.length; i++) {
+		if (invalidChar.has(folderName[i])) {
+			continue
+		}
+		newFolderName += folderName[i]
+	}
+	return newFolderName
+}
+
+//This takes the content type value (Ex: application/json) and returns the extensionName(json).
+function mediaTypeExtensionName(mediaTypeHTTPHeaderValue) {
+	let extensionName = ''
+	if (mediaTypeHTTPHeaderValue == undefined) {
+		return extensionName
+	}
+	for (let i = mediaTypeHTTPHeaderValue.length - 1; i >= 0; i--) {
+		if (mediaTypeHTTPHeaderValue[i] == '/') {
+			break
+		}
+		extensionName = mediaTypeHTTPHeaderValue[i] + extensionName
+	}
+	return extensionName.toLowerCase()
+}
+
+//parseURL takes a url and returns  extensionName and the fileName in a object.
+function parseURL(url) {
+	let obj = {
+		fileName: '',
+		extensionName: '',
+	}
+
+	url = new URL(url).pathname.trimEnd()
+
+	for (let i = url.length - 1; i >= 0; i--) {
+		if (url[i] == '.' && obj.extensionName == '') {
+			obj.extensionName = obj.fileName
+			obj.fileName = ''
+			continue
+		} else if (url[i] == '/') {
+			break
+		}
+		obj.fileName = url[i] + obj.fileName
+	}
+	obj.fileName = escapeFolderName(obj.fileName)
+	obj.extensionName = escapeFolderName(obj.extensionName)
+	return obj
+}
+
+function getHeaderValue(headers, headerName) {
+	for (let i = 0; i < headers.length; i++) {
+		if (headers[i].name.toLowerCase() == headerName.toLowerCase()) {
+			if (headers[i].value == null) {
+				break
+			}
+			return headers[i].value
+		}
+	}
+	return ''
+}
+
+function getContentTypeHeaderValue(headers) {
+	return getHeaderValue(headers, 'Content-Type')
+}
+
+function randomString(length) {
+	var result = ''
+	var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+	var charactersLength = characters.length
+	for (var i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength))
+	}
+	return result
+}
+
+function transformDetails(details, headers) {
+	let obj = {
+		fileName: '',
+		extensionName: '',
+		url: details.url,
+		method: details.method,
+		statusCode: details.statusCode,
+		type: details.type,
+		tabId: details.tabId,
+		contentLength: getHeaderValue(details.responseHeaders, 'Content-Length'),
+		headers: headers,
+	}
+
+	let contentTypeHederValue = getContentTypeHeaderValue(details.responseHeaders)
+	if (contentTypeHederValue == null) {
+		return obj
+	}
+	let urlObj = parseURL(details.url)
+	if (urlObj.extensionName == '') {
+		urlObj.extensionName = mediaTypeExtensionName(parseHeaderValue(contentTypeHederValue)[0])
+	}
+	if (urlObj.fileName == '') {
+		urlObj.fileName = randomString(5)
+	}
+	obj.extensionName = urlObj.extensionName
+	obj.fileName = urlObj.fileName
+
+	return obj
+}
+
 let webRequests = {
 	arrayCap: 256,
 	webRequestsContainer: new Map(),
